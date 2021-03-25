2a77db50cbfef24490958fd1856a6a70acd926b6
Kiarash Kiani <hi@kiarash.info>
2021-03-25 19:09:00 +0430

    remove softmax layer

--- model.py
+++ model.py
@@ -9,11 +9,12 @@ class GraphConvolutionalNetwork(nn.Module):
         super().__init__()
 
         self.gcl1 = GraphConv(in_feat, h_feat)
+        self.relu = nn.ReLU()
         self.gcl2 = GraphConv(h_feat, out_feat)
 
     def forward(self, graph, in_feat):
         x = self.gcl1(graph, in_feat)
-        x = F.relu(x)
+        x = self.relu(x)
         x = self.gcl2(graph, x)
 
-        return F.softmax(x)
+        return x
