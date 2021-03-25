1b1fd6f6061bf16ea5e7288877bf5353373027d0
Kiarash Kiani <hi@kiarash.info>
2021-03-25 12:47:16 +0430

    init Graph Neural Network Model

---
+++ model.py
@@ -0,0 +1,19 @@
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+from dgl.nn import GraphConv
+
+
+class GraphConvolutionalNetwork(nn.Module):
+    def __init__(self, in_feat, h_feat, out_feat):
+        super().__init__()
+
+        self.gcl1 = GraphConv(in_feat, h_feat)
+        self.gcl2 = GraphConv(h_feat, out_feat)
+
+    def forward(self, graph, in_feat):
+        x = self.gcl1(graph, in_feat)
+        x = F.relu(x)
+        x = self.gcl2(graph, x)
+
+        return F.softmax(x)
